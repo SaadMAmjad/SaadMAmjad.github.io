0ac101e5b11bb3e53811ac37da887397276ee470
SaadMAmjad <55886212+SaadMAmjad@users.noreply.github.com>
2023-07-02 04:53:11 -0400

    Added an empty accordian for showcasing projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
             {/* container for the picture and opening title */}
             <div className='container p-4 transparentAndColour'>
                 <div className='row'>
-                    {/* Container class by default will take only some of the screen on big devices. add -fluid to it for fullscreen
+                    {/* Container className by default will take only some of the screen on big devices. add -fluid to it for fullscreen
                     -------------------------------------------------------------------------
                     bootstrap works on a grid system, there are 12 total, this will take 4 of those
                     col-sm is for small screen (phones and stuff). col-md is for medium and col-lg for large
@@ -51,100 +51,150 @@ function Home() {
             </div>
 
             {/* padding temporary to make it fill the page */}
-            <div className='container transparentAndColour pb-2' style={{marginTop: '10rem'}}>
-            <h1 className='text-light text-center p-3'>Programming</h1>
-            <h3 className='p-2 secondaryText'>Languages and Markup</h3>
-            {/* Programming language table */}
-            <table class="table table-dark table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Technology</th>
-                        <th scope="col">Experience Level</th>
-                        <th scope="col" style={{width: '25%'}}>Notes</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className='align-middle'>
-                        <th scope="row">HTML and CSS</th>
-                        <td>Proficient</td>
-                        <td>Experienced with Bootstrap too</td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">PHP</th>
-                        <td>Proficient</td>
-                        <td></td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">SQL</th>
-                        <td>Some Experience</td>
-                        <td>I have used it quite a lot within Laravel's query builder</td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">Java</th>
-                        <td>Proficient</td>
-                        <td>Only base Java, I have not tried any frameworks so far</td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">Javascript</th>
-                        <td>Some Experience</td>
-                        <td></td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">Swift</th>
-                        <td>Some Experience</td>
-                        <td>I don't personally have a mac, so a bit rusty</td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">C#</th>
-                        <td>Some Experience</td>
-                        <td></td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">Ruby</th>
-                        <td>Only Dabbled</td>
-                        <td>Used it for one class in college</td>
-                    </tr>
-                </tbody>
-            </table>
-            <h3 className='p-2 secondaryText'>Frameworks and Platforms</h3>
-            {/* framework table */}
-            <table class="table table-dark table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Technology</th>
-                        <th scope="col">Experience Level</th>
-                        <th scope="col" style={{width: '25%'}}>Notes</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className='align-middle'>
-                        <th scope="row">React.js</th>
-                        <td>Some Experience</td>
-                        <td>Used to build this site</td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">Android (Java)</th>
-                        <td>Proficient</td>
-                        <td>A bit rusty</td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">iOS and XCode</th>
-                        <td>Some Experience</td>
-                        <td>No personal Mac so it's been some time since I worked with it</td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">Laravel (PHP)</th>
-                        <td>Proficient</td>
-                        <td></td>
-                    </tr>
-                    <tr className='align-middle'>
-                        <th scope="row">Ruby on Rails</th>
-                        <td>Only Dabbled</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className='container transparentAndColour pb-2' style={{marginTop: '15rem'}}>
+                <h1 className='text-light text-center p-3'>Programming Skills</h1>
+                <h3 className='p-2 secondaryText'>Languages and Markup</h3>
+                {/* Programming language table */}
+                <table className="table table-dark table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Technology</th>
+                            <th scope="col">Experience Level</th>
+                            <th scope="col" style={{width: '25%'}}>Notes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className='align-middle'>
+                            <th scope="row">HTML and CSS</th>
+                            <td>Proficient</td>
+                            <td>Experienced with Bootstrap too</td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">PHP</th>
+                            <td>Proficient</td>
+                            <td></td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">SQL</th>
+                            <td>Some Experience</td>
+                            <td>I have used it quite a lot within Laravel's query builder</td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">Java</th>
+                            <td>Proficient</td>
+                            <td>Only base Java, I have not tried any frameworks so far</td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">Javascript</th>
+                            <td>Some Experience</td>
+                            <td></td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">Swift</th>
+                            <td>Some Experience</td>
+                            <td>I don't personally have a mac, so a bit rusty</td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">C#</th>
+                            <td>Some Experience</td>
+                            <td></td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">Ruby</th>
+                            <td>Only Dabbled</td>
+                            <td>Used it for one className in college</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h3 className='p-2 secondaryText'>Frameworks and Platforms</h3>
+                {/* framework table */}
+                <table className="table table-dark table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Technology</th>
+                            <th scope="col">Experience Level</th>
+                            <th scope="col" style={{width: '25%'}}>Notes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className='align-middle'>
+                            <th scope="row">React.js</th>
+                            <td>Some Experience</td>
+                            <td>Used to build this site</td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">Android (Java)</th>
+                            <td>Proficient</td>
+                            <td>A bit rusty</td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">iOS and XCode</th>
+                            <td>Some Experience</td>
+                            <td></td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">Laravel (PHP)</th>
+                            <td>Proficient</td>
+                            <td></td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">Ruby on Rails</th>
+                            <td>Only Dabbled</td>
+                            <td></td>
+                        </tr>
+                        <tr className='align-middle'>
+                            <th scope="row">Git and GitHub</th>
+                            <td>Proficient</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
+            {/* programming projects section */}
+            <div className='container transparentAndColour pb-2 mt-5'>
+                <h1 className='text-light text-center p-3'>Programming Projects</h1>
+                <div className="accordion" id="accordionExample">
+                    {/* accordian trigger (button), can put other stuff in here too, like images or text */}
+                    <div className="accordion-item bg-dark text-white">
+                        <h2 className="accordion-header" id="headingOne">
+                            <button className="accordion-button bg-dark text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Accordion Item #1
+                            </button>
+                        </h2>
+                        {/* accordian body, what will actually be shown when clicked. need to figure out how to put vids in here sometime */}
+                        <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div className="accordion-body">
+                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item bg-dark text-white">
+                        <h2 className="accordion-header" id="headingTwo">
+                            <button className="accordion-button collapsed bg-dark text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Accordion Item #2
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div className="accordion-body">
+                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item bg-dark text-white">
+                        <h2 className="accordion-header" id="headingThree">
+                            <button className="accordion-button collapsed bg-dark text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Accordion Item #3
+                            </button>
+                        </h2>
+                        <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div className="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* other skills section and projects will follow this */}
             {/* Skills section */}
             {/* <div className='container mt-5 justify-content-between'>
                 <div className='row'>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,8 @@
 function Navbar() {
     return (
-        // TODO, navbar don't work with the context menu on mobile. figure out why
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3 fixed-top">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
