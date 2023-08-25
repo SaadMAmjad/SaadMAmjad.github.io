745c356530d6bdd09c05b61727724157aba33636
SaadMAmjad <55886212+SaadMAmjad@users.noreply.github.com>
2023-08-25 07:00:25 -0400

    Fixed a typo

--- src/components/ProProjects.js
+++ src/components/ProProjects.js
@@ -172,7 +172,7 @@ function ProProjects() {
                     <div className="accordion-item bg-dark text-white">
                         <h2 className="accordion-header" id="headingFour">
                             <button className="accordion-button collapsed bg-dark text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                <strong className='secondaryText'>GUI Webscrapper (New)</strong>
+                                <strong className='secondaryText'>GUI Webscraper (New)</strong>
                             </button>
                         </h2>
                         <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
